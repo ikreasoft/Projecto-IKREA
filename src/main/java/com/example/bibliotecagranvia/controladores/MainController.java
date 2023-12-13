a40223c21265b93f2e048686f5fcb115547c451a
oumdar <oumaimaderkaoui2@gmail.com>
2023-12-13 13:52:13 +0100

    Apatacion a los cambios de las clases Titulo y Autor

--- src/main/java/com/example/bibliotecagranvia/controladores/MainController.java
+++ src/main/java/com/example/bibliotecagranvia/controladores/MainController.java
@@ -2,6 +2,7 @@ package com.example.bibliotecagranvia.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,12 +10,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.bibliotecagranvia.entidades.Autor;
 import com.example.bibliotecagranvia.entidades.Titulo;
+import com.example.bibliotecagranvia.persistencia.AutorRepositorio;
 import com.example.bibliotecagranvia.persistencia.TituloRepositorio;
 
 @Controller
 public class MainController {
-    @Autowired
+   @Autowired
     TituloRepositorio tituloRepositorio;
+    @Autowired
+    AutorRepositorio autorRepositorio;
+
     @GetMapping("/")
     public String showHomePage(Model model){
         model.addAttribute("laberinto", "/img/labirinto1.jpg");
@@ -24,22 +29,33 @@ public class MainController {
     @GetMapping("/addTitle")
     public String addTitle(Model model){
         model.addAttribute("titulo", new Titulo());
-        //model.addAttribute("autores", autorRepositorio.findAll());
+        model.addAttribute("autoresTodos", autorRepositorio.findAll());
         return "addTitle";
     }
     @PostMapping("/addTitle")
     public String addTitlePost(@ModelAttribute Titulo titulo,Model model){
-           // titulo.setAutor(autorRepositorio.findAll().iterator().next());
-       // titulo.setAutor(autorRepositorio.findById(autore.getId()).get());
-       //titulo.addAutor(new Autor("José Carlos","Gonz."));
         tituloRepositorio.save(titulo);
         return "redirect:/";
     }
-    @GetMapping("/deleteTitle/{id}")
-    public String deletTitle(@PathVariable("id") long id, Model model) {
-    Titulo titulo = tituloRepositorio.findById(id)
-      .orElseThrow(() -> new IllegalArgumentException("Id de titulo no valido:" + id));
-    tituloRepositorio.delete(titulo);
-    return "redirect:/";
-}
+    @GetMapping("/editTitle/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Titulo titulo = tituloRepositorio.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Id de titulo no valido:" + id));
+
+        model.addAttribute("titulo", titulo);
+        model.addAttribute("autoresTodos", autorRepositorio.findAll());
+        return "updateTitle";
+    }
+    @PostMapping("/editTitle/{id}")
+    public String updateTitlePost(@PathVariable("id") long id, Titulo titulo,
+                                  BindingResult result, Model model){
+        System.out.println(titulo.getNombre());
+        /*if (result.hasErrors()) {
+            titulo.setId(id);
+            return "updateTitle";
+        }*/
+        tituloRepositorio.save(titulo);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/example/bibliotecagranvia/entidades/Titulo.java
+++ src/main/java/com/example/bibliotecagranvia/entidades/Titulo.java
@@ -3,10 +3,14 @@ package com.example.bibliotecagranvia.entidades;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
@@ -15,64 +19,69 @@ import jakarta.persistence.Table;
 @Table(name = "titulo")
 public class Titulo {
 
-    @Id
-    @SequenceGenerator(name="yourSequenceGenerator", allocationSize=1)
-    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "yourTableGenerator")
-    private Long id;
-    private String nombre;
-    private String isbn;
-    private String numReservas;
-    @ManyToMany
-    private List<Autor> autores;
-    public Titulo(){
-        autores= new ArrayList<Autor>();
-    }
-    public Titulo(String nombre, String isbn, String numReserva, List<Autor> autor){
-        this.nombre=nombre;
-        this.isbn=isbn;
-        this.numReservas=numReserva;
-        this.autores=autor;
+        @Id
+        @SequenceGenerator(name="yourSequenceGenerator", allocationSize=1)
+        @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "yourTableGenerator")
+        @Column(name = "tituloId")
+        private Long id;
+        private String nombre;
+        private String isbn;
+        private int numReservas;
+        @ManyToMany
+        @JoinTable(
+                name = "TituloAutor",
+                joinColumns = @JoinColumn(name = "tituloId"),
+                inverseJoinColumns = @JoinColumn(name = "autor_id"))
+        private List<Autor> autores;
+        public Titulo(){
+            autores= new ArrayList<Autor>();
+        }
+        public Titulo(String nombre, String isbn, int numReserva, List<Autor> autor){
+            this.nombre=nombre;
+            this.isbn=isbn;
+            this.numReservas=numReserva;
+            this.autores=autor;
 
-    }
+        }
 
-    public Long getId() {
-        return id;
-    }
+        public Long getId() {
+            return id;
+        }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+        public void setId(Long tituloId) {
+            this.id = tituloId;
+        }
 
-    public String getNombre(){
-        return nombre;
-    }
-    public void setNombre(String nombre){
-        this.nombre=nombre;
-    }
+        public String getNombre(){
+            return nombre;
+        }
+        public void setNombre(String nombre){
+            this.nombre=nombre;
+        }
 
-    public String getIsbn() {
-        return isbn;
-    }
+        public String getIsbn() {
+            return isbn;
+        }
 
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
+        public void setIsbn(String isbn) {
+            this.isbn = isbn;
+        }
 
-    public String getNumReservas() {
-        return numReservas;
-    }
+        public int getNumReservas() {
+            return numReservas;
+        }
 
-    public void setNumReservas(String numReserva) {
-        this.numReservas = numReserva;
-    }
+        public void setNumReservas(int numReserva) {
+            this.numReservas = numReserva;
+        }
 
-    public List<Autor> getAutor(){
-        return autores;
+        public List<Autor> getAutor(){
+            return autores;
+        }
+        public void setAutor(List<Autor> autor){
+            this.autores=autor;
+        }
+        public void addAutor(Autor autor){
+            this.autores.add(autor);
+        }
     }
-    public void setAutor(List<Autor> autor){
-        this.autores=autor;
-    }
-    public void addAutor(Autor autor){
-        this.autores.add(autor);
-    }
-}
