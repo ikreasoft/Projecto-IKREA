39356c58ad4a9590601bf2076492f2c857346de9
chunche95 <37621958+chunche95@users.noreply.github.com>
2023-12-18 12:28:38 +0100

    Controlador de titulos actualizado

--- src/main/java/com/example/bibliotecagranvia/controladores/TituloController.java
+++ src/main/java/com/example/bibliotecagranvia/controladores/TituloController.java
@@ -64,14 +64,14 @@ public class TituloController {
             return "updateTitle";
         }*/
         tituloRepositorio.save(titulo);
-        return "redirect:/updateTitle";
+        return "title/updateTitle";
     }
      @GetMapping("/deleteTitle/{id}")
     public String deleteUser(@PathVariable("id") long id, Model model) {
         Titulo titulo = tituloRepositorio.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Id de titulo no valido:" + id));
         tituloRepositorio.delete(titulo);
-        return "redirect:readTitles";
+        return "title/updateTitle";
     }
 
 
